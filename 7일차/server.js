ce11ce0a255cca9e2b5e80946d99bdfddc2f49a7
pia222kr20240629 <pia222.kr.20240629@gmail.com>
2024-08-10 15:52:42 +0900

    Add files via upload

--- 7일차/server.js
+++ 7일차/server.js
@@ -7,18 +7,20 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static('public'));
-io.on('connection',(socket) => {
-	console.log("New user connected");
-	socket.on('message', (msg) => {
-		io.emit('message', msg);
-	});
 
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
-	});
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('message', (msg) => {
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-	console.log('listening on : 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
